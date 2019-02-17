020f561c1c797409b65d407ba5d48e7443de1c48
Tracy Le <tnle9@asu.edu>
2019-02-16 19:26:11 -0800

    Exception handled movie search typos

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # liri-node-app
+Liri-node-app functions like Apple's SIRI, Speech Interpretation and Recognition Interface but LIRI is a *Language* Interpretation and Recognition Interface. LIRI will be a command line Node app that takes in parameters and gives you back data that you are searching for.
+
+## What LIRI uses to search for you:
+- Spotify for songs
+- Bands in Town for concerts (with Axios)
+- OMDB for movies  (with Axios)
+
+## Instructions 
+Open your Terminal and you can begin your song, concert, or movie searches. Each command requires different parameters, so be sure to include these in your search:
+
+## Bands in Town for concerts
+`node lirir.js concert-this <artist/band name here> `
+
+The search will show you something like this:
+
+## Spotify for songs
+`node lirir.js spotify-this-song <song name here> `
+
+The search will show you something like this:
+
+## OMDB for movies 
+`node lirir.js movie-this <movie title here> `
+
+The search will show you something like this:
+
+
--- liri.js
+++ liri.js
@@ -11,29 +11,14 @@ var fs = require("fs");
 var thirdCommand = process.argv[2];
 var fourthCommand = process.argv[3];
 
-if (thirdCommand === 'concert-this') {
-  axios.get("https://rest.bandsintown.com/artists/" + fourthCommand + "/events?app_id=codingbootcamp")
-  .then(function (response) {
-    var data = response.data;
-    for(var i = 0; i < data.length; i++) {
-      console.log("Venue: " + data[i].venue.name);
-      console.log("Location: " + data[i].venue.city + ", " + data[i].venue.country);
-      console.log("Date: " + moment(data[i].datetime).format("MM/DD/YYYY"));
-      console.log();
-    }
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-} 
-else if (thirdCommand === 'spotify-this-song') {
-  spotify.search({ type: 'track', query: fourthCommand }, function(err, response) {
+// Spotfy search
+function spotifySearch(song) {
+  spotify.search({ type: 'track', query: song }, function(err, response) {
+   
     if (err) {
       return console.log('Error occurred: ' + err);
     }
-    else if (fourthCommand === undefined) {
-      console.log("Name of album: The Sign (US Album) [Remastered] \nArtist: Ace of Base \nPreview link: https://p.scdn.co/mp3-preview/4c463359f67dd3546db7294d236dd0ae991882ff?cid=74cf423243964f9bb0f9e85abfe1db49 \nSong: The Sign");
-    }
+    
     var items = response.tracks.items;
     for(var i = 0; i < items.length; i++ ) {
       // Name of the album (works)
@@ -50,36 +35,97 @@ else if (thirdCommand === 'spotify-this-song') {
       console.log("Song: " + items[i].name);
       // Divider
       console.log("-------");
-
     }
    
-    
   })
+}
+// The Sign default search
+function theSignSearch() {
+  spotify.search({ type: 'track', query: "The Sign" }, function(err, response) {
+   
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+    
+    var items = response.tracks.items;
+    for(var i = 0; i < items.length; i++ ) {
+      if ("The Sign" === items[i].name) {
+        // Name of the album (works)
+        console.log("Name of album: " + items[i].album.name);
+
+        // Name of the artist (works) (need to loop through artists)
+        var artists = items[i].artists;
+        for(var j = 0; j < artists.length; j++) {
+          console.log("Artist: " + artists[j].name);
+        }
+        // Preview link from Spotify
+        console.log("Preview link: " + items[i].preview_url);
+        // Song name
+        console.log("Song: " + items[i].name);
+        // Divider
+        console.log("-------");
+      }
+    }
+  })
+}
+// Movie search
+function movieSearch(movieName){
+  axios.get("http://www.omdbapi.com/?t=" + movieName + "&apikey=b5f5e0a1")
+    .then(function(response){
+      var jsonData = response.data;
+      if (jsonData.Error === 'Movie not found!') {
+        console.log(jsonData.Error);
+        return;
+      }
+      var movieInfo = [
+        "Title: " + jsonData.Title,
+        "Year: " + jsonData.Year,
+        "Rotten Tomatoes: " + jsonData.Ratings[1].Value,
+        "IMBD Rating: " + jsonData.imdbRating,
+        "Country: " + jsonData.Country,
+        "Language: " + jsonData.Language,
+        "Plot: " + jsonData.Plot
+      ].join("\n\n");
+      console.log(movieInfo);
+    })
+    .catch(function (error){
+      console.log(error);
+    });
+}
+// Concert search 
+function concertSearch(artistName) {
+  axios.get("https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=codingbootcamp")
+  .then(function (response) {
+    var data = response.data;
+    for(var i = 0; i < data.length; i++) {
+      console.log("Venue: " + data[i].venue.name);
+      console.log("Location: " + data[i].venue.city + ", " + data[i].venue.country);
+      console.log("Date: " + moment(data[i].datetime).format("MM/DD/YYYY"));
+      console.log();
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
+if (thirdCommand === 'concert-this') {
+  concertSearch(fourthCommand);
+} 
+else if (thirdCommand === 'spotify-this-song') {
+  if (fourthCommand === undefined){
+    theSignSearch();
+    return;
+  }
+  spotifySearch(fourthCommand);
 } 
 else if (thirdCommand === 'movie-this') {
   if (fourthCommand === undefined) {
     console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/ \nIt's on Netflix!");
   } else {
-      axios.get("http://www.omdbapi.com/?t=" + fourthCommand + "&apikey=b5f5e0a1")
-      .then(function(response){
-
-        var jsonData = response.data;
-        var movieInfo = [
-          "Title: " + jsonData.Title,
-          "Year: " + jsonData.Year,
-          "Rotten Tomatoes: " + jsonData.Ratings[1].Value,
-          "IMBD Rating: " + jsonData.imdbRating,
-          "Country: " + jsonData.Country,
-          "Language: " + jsonData.Language,
-          "Plot: " + jsonData.Plot
-        ].join("\n\n");
-        console.log(movieInfo);
-      })
-      .catch(function (error){
-        console.log(error);
-      });
+    movieSearch(fourthCommand);
   }
 } 
+// fs node package
 else if (thirdCommand === 'do-what-it-says') {
   fs.readFile("random.txt", "utf8", function(error, data){
     if (error) {
@@ -88,5 +134,14 @@ else if (thirdCommand === 'do-what-it-says') {
     console.log(data);
     var dataArr = data.split(",");
     console.log(dataArr);
+    if (dataArr[0] === "spotify-this-song") {
+      spotifySearch(dataArr[1]);
+    }
+    else if (dataArr[0] === "concert-this") {
+      concertSearch(dataArr[1]);
+    }
+    else if (dataArr[0] === "movie-this") {
+      movieSearch(dataArr[1]);
+    };
   })
 }
